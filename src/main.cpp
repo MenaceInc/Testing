74d5b2b1d1280226350a7b8f758cc0e4394d73be
John Murray <MenaceInc@users.noreply.github.com>
2018-11-10 22:03:55 +0000

    Simple hello world but updated CMakeLists.txt and .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,6 @@
-#include <cstdint>
 #include <iostream>
 
-uint8_t classic_mode = 0b00000001;
-uint8_t vsync = 0b00000010;
-uint8_t capture_mouse = 0b00000100;
-uint8_t VR = 0b00001000;
-uint8_t raw_input = 0b00010000;
-uint8_t server_mode = 0b00100000;
-
-void printOptions(uint8_t options) {
-    if(options & classic_mode) {
-        std::cout << "Classic mode is enabled" << std::endl;
-    }
-    if(options & vsync) {
-        std::cout << "Vsync is enabled" << std::endl;
-    }
-    if(options & capture_mouse) {
-        std::cout << "Capture mouse is enabled" << std::endl;
-    }
-    if(options & VR) {
-        std::cout << "VR is enabled" << std::endl;
-    }
-    if(options & raw_input) {
-        std::cout << "Raw input is enabled" << std::endl;
-    }
-    if(options & server_mode) {
-        std::cout << "Server mode is enabled" << std::endl;
-    }
-}
-
-
-int main(void) {
-
-    uint8_t options = classic_mode | vsync | raw_input;
-
-    printOptions(options);
-    std::cout << "\nChanging options\n" << std::endl;
-
-    options ^= vsync | server_mode;
-
-    printOptions(options);
-    std::cout << "\nChanging options\n" << std::endl;
-
-    options ^= server_mode | raw_input | VR;
-
-    printOptions(options);
-
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, World!" << "\n";
     return 0;
 }
